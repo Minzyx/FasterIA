31967ef26f7280db6d6791d85eb84309b625df96
Minzyx <minzybtw@gmail.com>
2024-05-20 17:07:49 -0300

    feat: menu e formulário funcionais.

--- script.js
+++ script.js
@@ -35,3 +35,43 @@ window.onresize = () => {
     iconesX.style.display = "inline"
     iconeBarras.style.display = "none"
 }
+
+const solicitarOrcamento = (event) => {
+    // pegar valores dos inputs
+    let valorNome = document.getElementById("campo-nome").value
+    let valorEmail = document.getElementById("campo-email").value
+    let valorDescricao = document.getElementById("campo-descricao").value
+
+    // organizar objeto com os valores
+    let dadosForm = {
+        nome: valorNome,
+        email: valorEmail,
+        descricao: valorDescricao
+    }
+    // enviar requisição para a api
+    // 127.0.0.1 -> localhost
+    // Método HTTP POST - Create -> Cadastrar ou criar
+    fetch("http://127.0.0.1:3000/solicitacoes", {
+        method: "POST",
+        headers: {
+            "Content-Type": "application/json"
+        },
+        body: JSON.stringify(dadosForm)
+    })
+    .then(resposta => {
+        console.log(resposta)
+
+        // Limpar os campos
+        document.querySelector("#contato form").reset()
+
+        // Mostrar alerts com msg de sucesso
+        alert("Solicitação cadastrada")
+    })
+    .catch( erro => {
+        console.error(erro)
+        alert("Erro na requisição")
+    })
+
+
+   event.preventDefault()
+}
